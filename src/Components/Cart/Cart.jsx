0eb4380e4afef924459c363d359fecbd98f9f22e
muuhab <mohab.barca@gmail.com>
2021-05-09 06:53:31 +0200

    Cart buttons finished

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,42 +1,42 @@
 import React from 'react'
-import {Container,Typography,Button,Grid} from '@material-ui/core'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
 import useStyles from './styles'
 import CartItem from './CartItem/CartItem'
-import {Link} from 'react-router-dom'
-const Cart = ({cart}) => {
-    const classes=useStyles()
-    const EmptyCart=()=>{
+import { Link } from 'react-router-dom'
+const Cart = ({ cart, handleUpdateCartQty,handleRemoveFromCart,handleEmptyCart }) => {
+    const classes = useStyles()
+    const EmptyCart = () => {
         return <>
-        <Typography variant="subtitle1" >You have no items in the cart</Typography>
-        <Link to="/" className={classes.link}>Start adding some</Link>!
+            <Typography variant="subtitle1" >You have no items in the cart</Typography>
+            <Link to="/" className={classes.link}>Start adding some</Link>!
         </>
     }
-    const FilledCart=()=>{
+    const FilledCart = () => {
         return <>
-        <Grid container spacing={3}>
-            {cart.line_items.map((item)=>(
-                <Grid item xs={12} sm={4} key={item.id}>
-                    <CartItem item={item}></CartItem>
-                </Grid>
-            ))}
-        </Grid>
-        <div className={classes.cardDetails}>
-            <Typography variant="h4">
-                Subtotal:{cart.subtotal.formatted_with_symbol}
-            </Typography>
-            <Button variant="contained" size="large" color="secondary" type="button" className={classes.emptyButton}>Empty Cart</Button>
-            <Button variant="contained" size="large" color="primary" type="button" className={classes.checkoutButton}>Checkout</Button>
-        </div>
+            <Grid container spacing={3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <CartItem item={item} onUpdate={handleUpdateCartQty} onRemove={handleRemoveFromCart}></CartItem>
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant="h4">
+                    Subtotal:{cart.subtotal.formatted_with_symbol}
+                </Typography>
+                <Button variant="contained" size="large" color="secondary" type="button" className={classes.emptyButton} onClick={handleEmptyCart}>Empty Cart</Button>
+                <Button variant="contained" size="large" color="primary" type="button" className={classes.checkoutButton}>Checkout</Button>
+            </div>
         </>
     }
-    if(!cart.line_items) return 'loading...'
+    if (!cart.line_items) return 'loading...'
     return (
         <Container>
-            <div className={classes.toolbar}/>
+            <div className={classes.toolbar} />
             <Typography className={classes.title} variant="h3" gutterBottom >
-                Your Shopping Cart 
+                Your Shopping Cart
             </Typography>
-            {!cart.line_items.length? <EmptyCart/>:<FilledCart/>}
+            {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
 }
--- src/Components/Cart/CartItem/CartItem.jsx
+++ src/Components/Cart/CartItem/CartItem.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import {Typography,Button,Card,CardActions,CardContent,CardMedia} from '@material-ui/core'
 import useStyles from './styles'
 
-const CartItem = ({item}) => {
+const CartItem = ({item,onUpdate,onRemove}) => {
     const classes=useStyles()
     return (
         <Card>
            <CardMedia className={classes.media} image={item.media.source} alt={item.name} /> 
            <CardContent className={classes.cardContent}>
                <Typography variant="h4">{item.name}</Typography>
-               <Typography variant="h4">{item.line_total.formated_with_symbol}</Typography>
+               <Typography variant="h4">{item.line_total.formatted_with_symbol}</Typography>
            </CardContent>
            <CardActions className={classes.cartActions}>
                <div className={classes.buttons}>
-                   <Button tpye="button" size="small">-</Button>
+                   <Button tpye="button" size="small" onClick={()=>onUpdate(item.id,item.quantity-1)}>-</Button>
                    <Typography>{item.quantity}</Typography>
-                   <Button tpye="button" size="small">+</Button>
+                   <Button tpye="button" size="small" onClick={()=>onUpdate(item.id,item.quantity+1)}>+</Button>
                </div>
-               <Button variant="contained" color="secondary" type="button">Remove</Button>
+               <Button variant="contained" color="secondary" type="button" onClick={()=>onRemove(item.id)}>Remove</Button>
            </CardActions>
         </Card>
     )
