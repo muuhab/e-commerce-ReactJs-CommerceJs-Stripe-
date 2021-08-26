4b242a80e7286303493067b3ba1e8e0cf7b9ee79
muuhab <mohab.barca@gmail.com>
2021-08-26 22:54:30 +0200

    payment handled

--- src/Components/CheckoutForm/Checkout/Checkout.jsx
+++ src/Components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,64 +1,88 @@
-import React,{useState,useEffect} from 'react'
-import {Paper,Stepper,Step,StepLabel,Typography,CircularProgress,Divider,Button} from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline } from '@material-ui/core'
 import useStyles from './styles'
 import AddressForm from './AddressForm'
 import PaymentForm from './PaymentForm'
-import {commerce} from '../../../lib/commerce'
+import { commerce } from '../../../lib/commerce'
+import { Link,useHistory  } from 'react-router-dom'
 
-const steps=['Shipping address','Payment details']
+const steps = ['Shipping address', 'Payment details']
 
-const Checkout = ({cart}) => {
-    const classes=useStyles()
-    const [activeStep,setActiveStep]=useState(0)
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
+    const classes = useStyles()
+    const [activeStep, setActiveStep] = useState(0)
     const [checkoutToken, setCheckoutToken] = useState(null)
     const [shippingDetails, setShippingDetails] = useState({})
+    const history = useHistory();
 
-    useEffect(()=>{
-        const generateToken= async()=>{
-            try{
-                const token= await commerce.checkout.generateToken(cart.id,{type:'cart'})
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
                 console.log(token);
                 setCheckoutToken(token)
             }
-            catch(error){
+            catch (error) {
+                if (activeStep !== steps.length) history.push('/');
 
             }
         }
         generateToken()
-    },[cart])
-    const nextStep=()=>setActiveStep((prevActiveStep)=>prevActiveStep+1)
-    const prevStep=()=>setActiveStep((prevActiveStep)=>prevActiveStep-1)
-    
+    }, [cart])
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
+    const prevStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
 
-    const next=(data)=>{
+
+    const next = (data) => {
         setShippingDetails(data)
         nextStep()
     }
 
-    const Confirmation=()=>(
-        <div>
-            Confirmation
+    let Confirmation = () => (order.customer ? (
+        <>
+          <div>
+            <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}!</Typography>
+            <Divider className={classes.divider} />
+            <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+          </div>
+          <br />
+          <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+        </>
+      ) : (
+        <div className={classes.spinner}>
+          <CircularProgress />
         </div>
-    )
+      ));
+    
+      if (error) {
+        Confirmation = () => (
+          <>
+            <Typography variant="h5">Error: {error}</Typography>
+            <br />
+            <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+          </>
+        );
+      }
 
-    const Form=()=>activeStep===0
-    ?<AddressForm checkoutToken={checkoutToken} next={next}/>
-    :<PaymentForm shippingDetails={shippingDetails} checkoutToken={checkoutToken} back={prevStep}/>
+    const Form = () => activeStep === 0
+        ? <AddressForm checkoutToken={checkoutToken} next={next} />
+        : <PaymentForm shippingDetails={shippingDetails} checkoutToken={checkoutToken} nextStep={nextStep} back={prevStep} onCaptureCheckout={onCaptureCheckout} />
 
     return (
         <>
-            <div className={classes.toolbar}/>
+        {/* <CssBaseline/> */}
+            <div className={classes.toolbar} />
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant="h4" align="center">Checkout</Typography>
                     <Stepper activeStep={activeStep} className={classes.Stepper}>
-                        {steps.map((step)=>(
+                        {steps.map((step) => (
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
                             </Step>
                         ))}
                     </Stepper>
-                    {activeStep===steps.length?<Confirmation/>:checkoutToken&&<Form/>}
+                    {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
                 </Paper>
             </main>
         </>
--- src/Components/CheckoutForm/Checkout/PaymentForm.jsx
+++ src/Components/CheckoutForm/Checkout/PaymentForm.jsx
@@ -1,23 +1,57 @@
 import React from 'react'
-import {Typography,Button,Divider} from '@material-ui/core'
-import {Elements,CardElement,ElementsConsumer} from '@stripe/react-stripe-js'
-import {loadStripe} from '@stripe/stripe-js'
+import { Typography, Button, Divider,CssBaseline } from '@material-ui/core'
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
 import Review from './Review'
 
-const PaymentForm = ({checkoutToken,back}) => {
-    const stripePromise= loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+
+const PaymentForm = ({ shippingDetails, checkoutToken, back, onCaptureCheckout, nextStep }) => {
+
+    const handleSubmit = async (event, elements, stripe) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) return;
+
+        const cardElement = elements.getElement(CardElement);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
+
+        if (error) {
+            console.log('[error]', error);
+        } else {
+            const orderData = {
+                line_items: checkoutToken.live.line_items,
+                customer: { firstname: shippingDetails.firstName, lastname: shippingDetails.lastName, email: shippingDetails.email },
+                shipping: { name: 'International', street: shippingDetails.address1, town_city: shippingDetails.city, county_state: shippingDetails.shippingSubdivision, postal_zip_code: shippingDetails.zip, country: shippingDetails.shippingCountry },
+                fulfillment: { shipping_method: shippingDetails.shippingOption },
+                payment: {
+                    gateway: 'stripe',
+                    stripe: {
+                        payment_method_id: paymentMethod.id,
+                    },
+                },
+            };
+
+            onCaptureCheckout(checkoutToken.id, orderData);
+            nextStep();
+        }
+    }
+
     return (
         <>
-            <Review checkoutToken={checkoutToken}/>
-            <Divider/>
-            <Typography variant="h6" gutterBottom style={{margin:'20px 0'}} >Payment method</Typography>
+        <CssBaseline/>
+            <Review checkoutToken={checkoutToken} />
+            <Divider />
+            <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }} >Payment method</Typography>
             <Elements stripe={stripePromise} >
                 <ElementsConsumer>
-                    {({elements,stripe})=>(
-                        <form>
-                            <CardElement/>
+                    {({ elements, stripe }) => (
+                        <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
+                            <CardElement />
                             <br /><br />
-                            <div style={{display:'flex',justifyContent:'space-between'}}>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                                 <Button variant="outlined" onClick={back}>Back</Button>
                                 <Button disable={!stripe} variant="contained" color="primary" type="submit" >Pay {checkoutToken.live.subtotal.formatted_with_symbol}</Button>
                             </div>
